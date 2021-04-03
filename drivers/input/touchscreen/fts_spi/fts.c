52354e0397ee7d16d90b777cad65bc3c8ebb7aca
alk3pInjection <webmaster@raspii.tech>
2021-04-03 11:12:47 +0800

    drivers: input: touchscreen: Remove {lpm_disable, irq_boost} hacks
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: Ied1c10d0680eb9bfc20d1b7496a6f058018c0c46
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -154,13 +154,6 @@ static int fts_mode_handler(struct fts_ts_info *info, int force);
 static int fts_set_cur_value(int mode, int value);
 #endif
 extern int power_supply_is_system_supplied(void);
-#ifdef CONFIG_FTS_BOOST
-extern void touch_irq_boost(void);
-#endif
-#ifdef CONFIG_FTS_BOOST
-#define EVENT_INPUT 0x1
-extern void lpm_disable_for_dev(bool on, char event_dev);
-#endif
 #ifdef CONFIG_FTS_POWERSUPPLY_CB
 static int fts_write_charge_status(int status);
 #endif
@@ -193,9 +186,6 @@ void release_all_touches(struct fts_ts_info *info)
 	input_report_key(info->input_dev, BTN_INFO, 0);
 	mi_disp_set_fod_queue_work(0, true);
 	input_sync(info->input_dev);
-#ifdef CONFIG_FTS_BOOST
-	lpm_disable_for_dev(false, EVENT_INPUT);
-#endif
 	info->touch_id = 0;
 	info->touch_skip = 0;
 	info->fod_id = 0;
@@ -4112,9 +4102,6 @@ static void fts_leave_pointer_event_handler(struct fts_ts_info *info,
 		input_report_key(info->input_dev, BTN_TOUCH, touch_condition);
 		if (!touch_condition)
 			input_report_key(info->input_dev, BTN_TOOL_FINGER, 0);
-#ifdef CONFIG_FTS_BOOST
-		lpm_disable_for_dev(false, EVENT_INPUT);
-#endif
 
 		info->fod_pressed = false;
 		input_report_key(info->input_dev, BTN_INFO, 0);
@@ -4800,9 +4787,6 @@ static void fts_ts_sleep_work(struct work_struct *work)
 			logError(1, "%s pm_resume_completion timeout, i2c is closed", tag);
 			pm_relax(info->dev);
 			fts_enableInterrupt();
-#ifdef CONFIG_FTS_BOOST
-			lpm_disable_for_dev(false, EVENT_INPUT);
-#endif
 			return;
 		} else {
 			logError(1, "%s pm_resume_completion be completed, handling irq", tag);
@@ -4857,9 +4841,6 @@ static void fts_ts_sleep_work(struct work_struct *work)
 #endif
 	pm_relax(info->dev);
 	fts_enableInterrupt();
-#ifdef CONFIG_FTS_BOOST
-	lpm_disable_for_dev(false, EVENT_INPUT);
-#endif
 
 	return;
 }
@@ -4883,9 +4864,6 @@ static irqreturn_t fts_event_handler(int irq, void *ts_info)
 	static char pre_id[3];
 	event_dispatch_handler_t event_handler;
 
-#ifdef CONFIG_FTS_BOOST
-	touch_irq_boost();
-#endif
 	if (info->tp_pm_suspend) {
 		logError(1, "%s device in suspend, schedue to work", tag);
 		pm_wakeup_event(info->dev, 0);
@@ -4900,9 +4878,6 @@ static irqreturn_t fts_event_handler(int irq, void *ts_info)
 	if (!fts_secure_filter_interrupt(info)) {
 		return IRQ_HANDLED;
 	}
-#endif
-#ifdef CONFIG_FTS_BOOST
-	lpm_disable_for_dev(true, EVENT_INPUT);
 #endif
 	pm_stay_awake(info->dev);
 #ifdef TOUCH_THP_SUPPORT
@@ -4974,10 +4949,6 @@ end:
 		} else if (!info->touch_id)
 			info->clicktouch_count = info->clicktouch_num;
 	}
-#ifdef CONFIG_FTS_BOOST
-	if (!info->touch_id)
-		lpm_disable_for_dev(false, EVENT_INPUT);
-#endif
 #ifdef FTS_XIAOMI_TOUCHFEATURE
 	wake_up(&info->wait_queue);
 #endif
@@ -6811,9 +6782,6 @@ static void fts_suspend_work(struct work_struct *work)
 	info->sensor_sleep = true;
 	if (info->gesture_enabled || fts_need_enter_lp_mode())
 		fts_enableInterrupt();
-#ifdef CONFIG_FTS_BOOST
-	lpm_disable_for_dev(false, EVENT_INPUT);
-#endif
 	// xiaomi_touch_set_suspend_state(XIAOMI_TOUCH_SUSPEND);
 }
 
